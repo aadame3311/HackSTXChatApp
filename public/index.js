87f6053228d5f685bacb1e0a681ce2bc17ef58f5
aadame3311 <antonio.adame01@utrgv.edu>
2019-04-01 17:29:13 -0500

    added files

---
+++ public/index.js
@@ -0,0 +1,25 @@
+var socket = io.connect('http://localhost:3000');
+
+$('form.message-form').submit((e)=>{
+    e.preventDefault();
+
+    // Talk to server.
+    // 'chat message' could be called anything.
+    var msg = $('#m').val();
+    socket.emit('chat message', msg); 
+    $('#m').val(''); // Reset input text.
+
+    // Create DOM element. 
+    // Displays message sent.
+    var new_message = $(`<div class="message-container"><div class="self-message">[Name]: ${msg}</div></div>`);
+    $('.message-box').append(new_message);
+
+    return false;
+});
+
+// Do something when client receives a message. 
+socket.on('chat message', (msg)=>{
+    // Create new DOM element.
+    var new_message = $(`<div class="message-container"><div class="message">[Name]: ${msg}</div></div>`);
+    $('.message-box').append(new_message);
+})
---
+++ server/app.js
@@ -0,0 +1,40 @@
+
+const express = require('express'); // Link to Express.js
+const app = express();
+
+const http = require('http').Server(app); 
+
+const hostname = '127.0.0.1';
+const port = 3000; // Ports are where servers listen for information.
+const path = require('path'); // Path module.
+// Add static files on client/ to our path.
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Listen on port 3000, then use callback function to notify.
+var server = app.listen(port, ()=>console.log(`Example app listening on port ${port}`));
+const io = require('socket.io').listen(server);
+
+
+
+// Routes. 
+app.get('/', (req, res)=>{
+    
+    // Use built in path module to serve html.
+    res.sendFile(path.join(__dirname, '../public/main.html'));
+
+});
+
+// Socket.io stuff. 
+io.on('connection', (socket)=> {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+
+    socket.on('chat message', (msg) => {
+        // Send message to everyone connected to socket. 
+        // 'chat message' could be called anything...
+        // Here we want to keep the same name as we did when we emit from client.
+        socket.broadcast.emit('chat message', msg);
+    });
+})
+
+
